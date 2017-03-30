0571206d1c6b0eb218cd5d241f49ff2100530bd0
Dan Armstrong <dan.d.armstrong@me.com>
2017-03-30 12:52:33 -0600

    Fix for status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,8 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.overlaysWebView(true);
+      StatusBar.styleLightContent();
       //StatusBar.styleBlackTranslucent();
       Splashscreen.hide();
     });
